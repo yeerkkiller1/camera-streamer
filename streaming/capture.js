e9af4778f8634008a19e8bf02c7076d3e5e65e76
yeerkkiller1 <yeerkkiller1@gmail.com>
2018-05-25 04:21:09 -0400

    save

--- streaming/capture.js
+++ streaming/capture.js
@@ -24,41 +24,14 @@ function addFrameTime() {
     if(rollingFrames.length > rollingFrameCount) {
         rollingFrames.shift();
         let FPmS = rollingFrameCount / (rollingFrames[rollingFrameCount - 1] - rollingFrames[0]);
-        //console.log("FPS", (FPmS * 1000).toFixed(3));
+        console.log("FPS", (FPmS * 1000).toFixed(3));
     }
 }
-
-let lastDigest = "";
-let firstChars = 5;
-let lastChars = 30;
 
 cam.start();
 cam.capture(function onCapture(success) {    
     var frame = cam.frameRaw();
     let buffer = Buffer.from(frame);
-    let hash = crypto.createHash("sha256");
-    hash.write(buffer);
-    hash.end();
-    let digest = hash.digest("base64");
-    lastDigest = lastDigest || digest;
-
-    let startPart = lastDigest.slice(0, firstChars);
-    let endPart = lastDigest.slice(-lastChars);
-    let mutableDigestPart = digest.slice(firstChars, -lastChars);
-
-    if(!digest.startsWith(startPart)) {
-        console.log("start changed");
-        console.log(digest, mutableDigestPart);
-        lastDigest = digest;
-    }
-
-    if(!digest.endsWith(endPart)) {
-        console.log("end changed");
-        console.log(digest);
-        lastDigest = digest;
-    }
-    
-  
     addFrameTime();
     cam.capture(onCapture);
 });
