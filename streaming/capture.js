a4906fb12d14bc834f3bc504de2ca7c2408472a8
yeerkkiller1 <yeerkkiller1@gmail.com>
2018-05-25 03:35:16 -0400

    save

--- streaming/capture.js
+++ streaming/capture.js
@@ -1,7 +1,9 @@
 var v4l2camera = require("v4l2camera");
 
 var cam = new v4l2camera.Camera("/dev/video0");
-cam.configSet(cam.formats.filter(x => x.formatName === "MJPG")[0]);
+let format = cam.formats.filter(x => x.formatName === "MJPG")[0];
+console.log(format);
+cam.configSet(format);
 if (cam.configGet().formatName !== "MJPG") {
   console.log("NOTICE: MJPG camera required");
   process.exit(1);
@@ -9,6 +11,7 @@ if (cam.configGet().formatName !== "MJPG") {
 cam.start();
 cam.capture(function (success) {
   var frame = cam.frameRaw();
-  require("fs").createWriteStream("result.jpg").end(new Buffer(frame));
+  let buffer = Buffer.from(frame);
+  require("fs").createWriteStream("result.jpg").end(buffer);
   cam.stop();
 });
